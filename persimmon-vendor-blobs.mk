72ead83e0a3006b4f303d763a08e5763a6162d1a
olegsvs <oleg.texet@gmail.com>
2016-12-06 12:06:21 +0000

    camera port from venree thor

--- persimmon-vendor-blobs.mk
+++ persimmon-vendor-blobs.mk
@@ -22,7 +22,6 @@ PRODUCT_COPY_FILES += \
 	vendor/archos/persimmon/proprietary/lib/libcam.iopipe_FrmB.so:system/lib/libcam.iopipe_FrmB.so \
 	vendor/archos/persimmon/proprietary/lib/libcamdrv_FrmB.so:system/lib/libcamdrv_FrmB.so \
 	vendor/archos/persimmon/proprietary/lib/libcamera_metadata.so:system/lib/libcamera_metadata.so \
-	vendor/archos/persimmon/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
 	vendor/archos/persimmon/proprietary/lib/libimageio_FrmB.so:system/lib/libimageio_FrmB.so \
 	vendor/archos/persimmon/proprietary/lib/libimageio_plat_drv_FrmB.so:system/lib/libimageio_plat_drv_FrmB.so \
     vendor/archos/persimmon/proprietary/lib/libbtcust.so:system/lib/libbtcust.so \
